864c277140bec0fa03e006a93b835b5b85f84753
Salman S <salkuadrat@gmail.com>
2021-05-26 07:19:51 +0700

    edit learning

--- lib/src/learning.dart
+++ lib/src/learning.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
 
+import 'package:learning_barcode_scanning/learning_barcode_scanning.dart';
 import 'package:learning_entity_extraction/learning_entity_extraction.dart'
     as LEE;
 import 'package:learning_face_detection/learning_face_detection.dart';
+import 'package:learning_image_labeling/learning_image_labeling.dart';
 import 'package:learning_input_image/learning_input_image.dart';
 import 'package:learning_language/learning_language.dart';
+import 'package:learning_object_detection/learning_object_detection.dart';
+import 'package:learning_pose_detection/learning_pose_detection.dart';
+import 'package:learning_selfie_segmentation/learning_selfie_segmentation.dart';
 import 'package:learning_text_recognition/learning_text_recognition.dart';
 import 'package:learning_translate/learning_translate.dart';
 import 'package:learning_smart_reply/learning_smart_reply.dart';
@@ -78,5 +83,57 @@ class ML {
     List<Face> result = await detector.detect(image);
     detector.dispose();
     return result;
+  }
+
+  static Future<Pose?> detectPose(InputImage image, {isStream: false}) async {
+    PoseDetector detector = PoseDetector(isStream: isStream);
+    Pose? result = await detector.detect(image);
+    detector.dispose();
+    return result;
+  }
+
+  static Future<List<DetectedObject>> detectObjects(
+    InputImage image, {
+    isStream: false,
+    enableClassification: true,
+    enableMultipleObjects: true,
+  }) async {
+    ObjectDetector detector = ObjectDetector(
+      isStream: isStream,
+      enableClassification: enableClassification,
+      enableMultipleObjects: enableMultipleObjects,
+    );
+
+    List<DetectedObject> result = await detector.detect(image);
+    detector.dispose();
+    return result;
+  }
+
+  static Future<List> scanBarcode(InputImage image,
+      {List<BarcodeFormat> formats = const []}) async {
+    BarcodeScanner scanner = BarcodeScanner(formats: formats);
+    List result = await scanner.scan(image);
+    scanner.dispose();
+    return result;
+  }
+
+  static Future<List<Label>> imageLabeling(InputImage image,
+      {double confidenceThreshold = 0.8}) async {
+    ImageLabeling imageLabeling = ImageLabeling();
+    List<Label> labels = await imageLabeling.process(image);
+    imageLabeling.dispose();
+    return labels;
+  }
+
+  static Future<SegmentationMask?> selfieSegmentation(
+    InputImage image, {
+    bool isStream = true,
+    bool enableRawSizeMask = false,
+  }) async {
+    SelfieSegmenter segmenter = SelfieSegmenter(
+        isStream: isStream, enableRawSizeMask: enableRawSizeMask);
+    SegmentationMask? mask = await segmenter.process(image);
+    segmenter.dispose();
+    return mask;
   }
 }
