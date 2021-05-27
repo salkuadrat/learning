79efe492ce0f1d1e7997eb40c784dc2cc04673a0
Salman S <salkuadrat@gmail.com>
2021-05-28 04:05:34 +0700

    edit learning translate ios

--- packages/learning_translate/ios/Classes/SwiftLearningTranslatePlugin.swift
+++ packages/learning_translate/ios/Classes/SwiftLearningTranslatePlugin.swift
@@ -9,6 +9,7 @@ public class SwiftLearningTranslatePlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "LearningTranslate", binaryMessenger: registrar.messenger())
     let instance = SwiftLearningTranslatePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    setModelManager(registrar);
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -34,11 +35,11 @@ public class SwiftLearningTranslatePlugin: NSObject, FlutterPlugin {
         details: nil))
       return
     }
-
+    
     let source: String? = args["from"]
     let target: String? = args["to"]
     let text: String? = args["text"]
-    let isDownloadRequireWifi: Bool = args["isDownloadRequireWifi"] ?? false
+    let isDownloadRequireWifi: Bool = args["isDownloadRequireWifi"] ?? true
     
     if source == nil || target == nil || text == nil {
       result(FlutterError(
@@ -76,6 +77,103 @@ public class SwiftLearningTranslatePlugin: NSObject, FlutterPlugin {
         
         result(translatedText)
       }
+    }
+  }
+
+  func setModelManager(registrar: FlutterPluginRegistrar) {
+    let modelManagerChannel = FlutterMethodChannel(name: "LearningTranslationModelManager", binaryMessenger: registrar.messenger())
+
+    modelManagerChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        if call.method == "list" {
+          listModel(result)
+        } else if call.method == "download" {
+          downloadModel(call, result)
+        } else if call.method == "check" {
+          checkModel(call, result)
+        } else if call.method == "delete" {
+          deleteModel(call, result)
+        } else {
+          result(FlutterMethodNotImplemented)
+        }
+    })
+  }
+
+  func listModel(result: FlutterResult) {
+    let modelManager = ModelManager.modelManager()
+    let models = modelManager.downloadedTranslateModels.map { $0.language.rawValue }
+    result(models)
+  }
+
+  func checkModel(call: FlutterMethodCall, result: FlutterResult) {
+    guard let args = call.arguments else {
+      result(FlutterError(
+        code: "NOARGUMENTS", 
+        message: "No arguments",
+        details: nil))
+      return
+    }
+
+    let language: String? = args["model"]
+    let modelManager = ModelManager.modelManager()
+
+    if language == nil {
+      result(false)
+      return
+    }
+
+    let model = TranslateRemoteModel.translateRemoteModel(language: language)
+    result(modelManager.isModelDownloaded(model))
+  }
+
+  func downloadModel(call: FlutterMethodCall, result: FlutterResult) {
+    guard let args = call.arguments else {
+      result(FlutterError(
+        code: "NOARGUMENTS", 
+        message: "No arguments",
+        details: nil))
+      return
+    }
+
+    let language: String? = args["model"]
+    let isDownloadRequireWifi: Bool = args["isDownloadRequireWifi"] ?? true
+    let modelManager = ModelManager.modelManager()
+
+    if language == nil {
+      result(false)
+      return
+    }
+
+    let model = TranslateRemoteModel.translateRemoteModel(language: language)
+    let conditions = ModelDownloadConditions(
+      allowsCellularAccess: !isDownloadRequireWifi,
+      allowsBackgroundDownloading: true
+    )
+
+    modelManager.download(model, conditions: conditions)
+    result(true)
+  }
+
+  func deleteModel(call: FlutterMethodCall, result: FlutterResult) {
+    guard let args = call.arguments else {
+      result(FlutterError(
+        code: "NOARGUMENTS", 
+        message: "No arguments",
+        details: nil))
+      return
+    }
+
+    let language: String? = args["model"]
+    let modelManager = ModelManager.modelManager()
+
+    if language == nil {
+      result(false)
+      return
+    }
+
+    let model = TranslateRemoteModel.translateRemoteModel(language: language)
+    modelManager.deleteDownloadedModel(model) { error in
+      result(true);
     }
   }
 }
