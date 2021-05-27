99a10d68351c18f1c672d897a52c28434694f742
Salman S <salkuadrat@gmail.com>
2021-05-27 21:24:30 +0700

    edit learning translate example

--- packages/learning_translate/example/lib/main.dart
+++ packages/learning_translate/example/lib/main.dart
@@ -55,6 +55,7 @@ class _TranslateTextPageState extends State<TranslateTextPage> {
   @override
   void dispose() {
     //_identifier.dispose();
+    _controller.dispose();
     _translator.dispose();
     super.dispose();
   }
